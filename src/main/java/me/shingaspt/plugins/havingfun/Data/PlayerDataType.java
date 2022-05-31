4dcab9eaab8f0f66c7e95f37f7ed12036a4ef44e
ShingasPT <afonsohenriques661@gmail.com>
2022-05-31 13:17:24 +0100

    ~ Fixed PlayerData Saving Error ~ Fixed All Italic Items in GUIS

---
+++ src/main/java/me/shingaspt/plugins/havingfun/Data/PlayerDataType.java
@@ -0,0 +1,9 @@
+package me.shingaspt.plugins.havingfun.Data;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerDataType extends TypeToken<Map<UUID, PlayerData>> {
+}
